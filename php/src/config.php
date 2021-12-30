e6a0d53e2389624d9471c5aacf32e8195a0978fa
= <=>
2021-12-30 12:08:34 +0700

    first commit

---
+++ php/src/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER','10.0.0.48');
+define('DB_USER','admin');
+define('DB_PASS' ,'Sumbermanjing20_');
+define('DB_NAME','pendaftaran');
+$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
